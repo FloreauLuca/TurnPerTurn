ef54cb75e901e6ed602275cffbaf9993e5633aa5
Tristan <tristan.schorr@hotmail.fr>
2019-10-14 04:30:41 +0200

    1ere version pkmn scriptable object

---
+++ TurnPerTurn/Assets/TurnBaseScript/pkmnScriptableObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Pokemon 1", menuName = "ScriptableObjects/PkmnScriptableObject", order = 1)]
+public class pkmnScriptableObj : ScriptableObject
+{
+    public int hp = 0;
+    public int speed = 0;
+    public int atk = 0;
+    public int def = 0;
+    public string ability1;
+    public string ability2;
+    public string abilitySwitch;
+}
