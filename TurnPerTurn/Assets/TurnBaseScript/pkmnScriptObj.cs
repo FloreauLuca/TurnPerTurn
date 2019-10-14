9bbac5d1288436c2d77fedcf663c91ab5393014c
Tristan <tristan.schorr@hotmail.fr>
2019-10-14 20:11:05 +0200

    1ere version ability scriptable object (les capacités de soin, dégat et boost)

--- TurnPerTurn/Assets/TurnBaseScript/pkmnScriptObj.cs
+++ TurnPerTurn/Assets/TurnBaseScript/pkmnScriptObj.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Pokemon 1", menuName = "ScriptableObjects/PkmnScriptableObject", order = 1)]
-public class pkmnScriptableObj : ScriptableObject
+public class pkmnScriptObj : ScriptableObject
 {
+    
     public int hp = 0;
     public int speed = 0;
     public int atk = 0;
     public int def = 0;
+    public string pkmnName;
     public string ability1;
     public string ability2;
     public string abilitySwitch;
