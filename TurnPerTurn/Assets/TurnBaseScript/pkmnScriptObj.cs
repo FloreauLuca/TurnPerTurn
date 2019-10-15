debd2669cf792d957bf67ae4ab394df051689c3d
Tristan <tristan.schorr@hotmail.fr>
2019-10-15 15:33:22 +0200

    premier test pkmn script obj

--- TurnPerTurn/Assets/TurnBaseScript/pkmnScriptObj.cs
+++ TurnPerTurn/Assets/TurnBaseScript/pkmnScriptObj.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "Pokemon 1", menuName = "ScriptableObjects/PkmnScriptableObject", order = 1)]
 public class pkmnScriptObj : ScriptableObject
-{
-    
+{  
     public int hp = 0;
     public int speed = 0;
     public int atk = 0;
     public int def = 0;
     public string pkmnName;
+    public string pkmnType;
     public string ability1;
     public string ability2;
-    public string abilitySwitch;
+    public bool pkmnSwitch;
 }
